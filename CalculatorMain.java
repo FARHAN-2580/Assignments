1f6f66ca4b53a851b90ea83bd5e264c2b1cda8ec
FARHAN-2580 <81003652+FARHAN-2580@users.noreply.github.com>
2021-12-27 11:19:32 +0530

    Update CalculatorMain.java

--- CalculatorMain.java
+++ CalculatorMain.java
@@ -5,22 +5,18 @@ public class CalculatorMain {
 		Calculator cal=new Calculator();
 		cal.addition(2, 2);
 		cal.addition(2.4, 9.6);
-		cal.addition(5.2, 6);
-		cal.addition(6, 8.3);
+		cal.addition(5.2f, 6f);
 		
 		cal.difference(15, 4);
 		cal.difference(7.3, 5.6);
-		cal.difference(9.8, 4);
-		cal.difference(8, 4.2);
-		
+		cal.difference(9.8f, 4f);
+
 		cal.multiplication(10, 9);
 		cal.multiplication(3.6, 5.5);
-		cal.multiplication(17.6, 5);
-		cal.multiplication(3, 8.3);
+		cal.multiplication(17.6f, 5f);
 		
-    cal.division(8, 5);
-    cal.division(11.9, 5.3);
-    cal.division(13.8, 6);
-    cal.division(20, 5.3);
+                cal.division(8, 5);
+                cal.division(11.9, 5.3);
+                cal.division(13.8f, 6f);
   }
 }
